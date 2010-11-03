71d03b19df357fc8cde5d1b497c1e901f476c222
Stéphane Akkaoui <stephane.akkaoui@imeuble.info>
2010-11-03 01:51:32 +0100

    Change to new layout

--- base_app.rb
+++ base_app.rb
@@ -7,21 +7,31 @@ get '/' do
 end
 
 get '/contact' do
+  @menu = ["contact", 'contact']
   @bread_crumbs = [['/contact', 'Contact']]
   erb :contact
 end
 
 get '/excursions' do
+  @menu = ["excursions", 'excursions']
   @bread_crumbs = [['/excursions', 'Excursions']]
   erb :excursions
 end
 
 get '/seminaires' do
+  @menu = ["incentive", 'seminaire']
   @bread_crumbs = [['/seminaires', 'Séminaires']]
   erb :seminaires
 end
 
 get '/x2' do
+  @menu = ["prez", 'x2']
   @bread_crumbs = [['/x2', 'Segway X2']]
   erb :x2
+end
+
+get '/i2' do
+  @menu = ["prez", 'i2']
+  @bread_crumbs = [['/i2', 'Segway I2']]
+  erb :i2
 end
